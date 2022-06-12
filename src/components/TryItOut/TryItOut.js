55c80f9c40f8f53b6deeae601292ccaa59454939
Bryan Dang <azangi700@gmail.com>
2022-06-12 16:35:01 -0400

    Added suggested links

--- src/components/TryItOut/TryItOut.js
+++ src/components/TryItOut/TryItOut.js
@@ -6,30 +6,37 @@ const TryItOut = ({ onInputChange, onButtonSubmit }) => {
 
   const prependLink = 'http://localhost:3001/'
 
-  const [apiInput, setApiInput] = useState(prependLink + "all");
+  const [apiInput, setApiInput] = useState("all");
   const [apiResponse, setApiResponse] = useState({});
 
   useEffect(() => {
     // send HTTP request
-    fetch(apiInput)
+    fetch(prependLink + apiInput)
     .then(response => response.json())
     .then(data => {
       setApiResponse(data)
-      console.log(apiResponse)
     })
   }, [])
   
   onInputChange = (event) => {
-    setApiInput(prependLink + event.target.value)
+    setApiInput(event.target.value)
   }
 
   onButtonSubmit = () => {
     // send HTTP request
-    fetch(apiInput) 
+    fetch(prependLink + apiInput) 
     .then(response => response.json())
     .then(data => {
       setApiResponse(data)
-      console.log(apiResponse)
+    })
+  }
+
+  const onClickLink = (value) => {
+    setApiInput(value)
+    fetch(prependLink + value) 
+    .then(response => response.json())
+    .then(data => {
+      setApiResponse(data)
     })
   }
 
@@ -41,15 +48,17 @@ const TryItOut = ({ onInputChange, onButtonSubmit }) => {
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           {/* TODO (bdang) Change this localhost */}
-          <span class="input-group-addon">http://localhost:3001/</span> 
-          <input className='f4 pa2 w-70 center' type='tex' defaultValue={"all"} onChange={onInputChange}/>
+          <span className="input-group-addon">http://localhost:3001/</span> 
+          <input className='f4 pa2 w-70 center' type='tex' value={apiInput} onChange={onInputChange}/>
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
           >Submit</button>
         </div>
         {/* TODO (bdang) Change these Lorem Ipsum things */}
-        <p className="help-text">Need some help? Try out these: Lorem Ipsum </p>
+        <p className="help-text">Need some help? Try out these: <a href="#" onClick={() => onClickLink('overview/')}>overview/</a> , 
+        <a href="#" onClick={() => onClickLink('cardEffects/cards/0/')}>cardEffects/cards/0/</a> ,  
+        <a href="#" onClick={() => onClickLink('setup/steps/0/')}>setup/steps/0/</a></p>
       </div>
 
       <div className='response pa4'>
@@ -63,5 +72,7 @@ const TryItOut = ({ onInputChange, onButtonSubmit }) => {
     </div>
   );
 }
+
+// TODO (bdang) When you hit the enter button, also submit
 
 export default TryItOut;
